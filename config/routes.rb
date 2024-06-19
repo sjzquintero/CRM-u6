f51a3cdb95bbc831e1d4dc301249d7f79d1f0157
Santiago Jimenez <squintero@rrc.ca>
2024-06-19 15:25:49 -0500

    created the customer view using image_tag

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  # Otras rutas...
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Define la ruta para la acción index del controlador Customers
+  resources :customers, only: [:index]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
