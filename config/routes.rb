f75d835dfb6bba4b4895a37b254ea41eb84d5ae4
Santiago Jimenez <squintero@rrc.ca>
2024-06-20 00:53:23 -0500

    commit with the actions/views and the configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  get 'customers/index'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  resources :customers, only: [:index] do
+    collection do
+      get 'alphabetized'
+      get 'missing_email'
+    end
+  end
+
+    root 'customers#index', as: 'home'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
